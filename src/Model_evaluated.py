1891a005ce55b3c9f6cf97b5e8e4e71572955e12
Amine Meskar <meskara@NB53BP3.local>
2025-06-30 22:18:42 +0200

    New Update

---
+++ src/Model_evaluated.py
@@ -0,0 +1,94 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+import zipfile
+import os, sys
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.base import RegressorMixin
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
+
+from Logger import logging
+from Exception import CustomizeExcep
+from src.Data_split import Split, Spliting_dat
+
+
+class ModelBuild(ABC):
+
+    def Model(self, X_train: pd.DataFrame, y_train: pd.Series):
+
+        """
+        
+        """
+
+        pass
+
+
+class LinearRegressionStartegy(ModelBuild):
+
+    def Model(self, X_train: pd.DataFrame, y_train: pd.Series):
+
+        """
+        
+        """
+
+        pass
+
+
+class XgBoostRegressorStartegy(ModelBuild):
+
+    def Model(self, X_train: pd.DataFrame, y_train: pd.Series):
+
+        """
+        
+        """
+
+        pass
+    
+class RFRegressionStartegy(ModelBuild):
+
+    def Model(self, X_train: pd.DataFrame, y_train: pd.Series):
+
+        """
+        
+        """
+
+        pass
+
+class file_ext: 
+
+    def __init__(self, strategy: ModelBuild):
+        """
+        
+        """
+        self._strategy = strategy
+
+    def set_strategy(self, strategy: ModelBuild):
+        """
+        
+        """
+        logging.info("Switching data splitting strategy.")
+        self._strategy = strategy
+
+    def fit_model(self, X_train: pd.DataFrame, y_train: pd.Series):
+        """
+        
+        """
+        logging.info("Splitting data using the selected strategy.")
+        return self._strategy.Model(X_train, y_train)
+    
+"""        
+if __name__ == '__main__': 
+  
+    df = pd.read_csv("/Users/meskara/Desktop/Github/Real_Estatet_endtoend/src/extracted_data/AmesHousing.csv") 
+
+    obj = Split(Spliting_dat()) 
+    
+    X_train, X_test, y_train, y_test = obj.split(df, 'SalePrice')
+
+    strategy = file_ext(LinearRegressionStartegy()) 
+    
+    pipeline = strategy.fit_model(X_train, y_train) """
